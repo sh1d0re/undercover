0729bef710e5d086d4e96674f4d6ed4fcc24d679
Shidore <82015990+sh1d0re@users.noreply.github.com>
2024-05-06 01:29:18 +0900

    Update model.py

--- models/model.py
+++ models/model.py
@@ -1,17 +1,32 @@
 terminology=[
-    [[5,"are"],[2,"r",""]],
-    [[5,"you"],[3,"yo","ya","u","you"]],
-    [[5,"doing"],[3,"doin","doing"]],
-    [[5,"what"],[2,"wut","wat"]],
-    [[5,"and"],[3,"&","and"]],
-    [[5,"with"],[2,"w/","wis","with"]],
-    [[5,"very","extremely","insanely"],[3,"very"],[2,"fucking","fuckin","goddam"]],
+    [[5,"are"],[2,"are","r","ar"]],
+    [[5,"you"],[2,"you","u","ya"]],
+    [[5,"doing"],[2,"doing","doin"]],
+    [[5,"what"],[2,"what","wut","wat"]],
+    [[5,"and"],[3,"and","&"]],
+    [[5,"with"],[2,"with","w/","wis","wiz"]],
+    [[5,"very","pretty"],[4,"very"],[3,"very","so"],[2,"fucking","fuckin","goddam"]],
     [[5,"one","1"],[2,"1"]],
+    [[5,"my"],[2,"my","ma"]],
+    [[5,"can not","cannot","cant"],[4,"cannot","cant"],[2,"cant"]],
+    [[5,"do not","dont"],[3,"dont"]],
     [[5,"likes"],[1,"like"]],
-    [[5,"your"],[2,"ur","yur","your"]],
+    [[5,"because"],[3,"because","cuz","becuz"]],
+    [[5,"its"],[2,"its","itz"]],
+    [[5,"youre"],[3,"your"],[2,"ur","yur","your"]],
+    [[5,"may"],[3,"can"]],
+    [[5,"love"],[2,"love","luv","❤️"]],
     [[5,"yours"],[3,"urs","yours"],[2,"urs"]],
+    [[5,"is"],[2,""]],
     [[5,"ok"],[2,"ok","fine"]],
-    [[5,"no"],[2,"nah","no"]]]
+    [[5,"no"],[2,"nah","no"]],
+    [[5,"oh"],[2,"oh","aw"]],
+    [[5,"see you"],[3,"cya","see you"]],
+    [[5,""],[2,""]],
+    [[5,""],[2,""]],
+    [[5,""],[2,""]],
+    [[5,""],[2,""]],
+    [[5,""],[2,""]]]
 
 # Swear / family friendly feature. Understand i gotta write some swearing under here. DO NOT TAKE ANY OFFENSE.
-maturity=[['fuck','frick','f**k','f*ck'],['goddam','godda*n',''],'damn','dam','darn','shit','shitting','bigass','ass','dick']
+maturity=[["fuck","frick","f**k","f*ck"],["goddam","godda*n","godd**n"],"damn","dam","darn","shit","shitting","bigass","ass","dick"]
